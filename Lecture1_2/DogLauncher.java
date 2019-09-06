2abad754425cf9e3b899a4ca21ff50da6ddc41f8
kevinchang95 <kevinchanguh@gmail.com>
2019-09-06 15:00:17 -0500

    Use instance array

--- Lecture1_2/DogLauncher.java
+++ Lecture1_2/DogLauncher.java
@@ -3,7 +3,7 @@ public class DogLauncher{
 		//Dog d = new Dog(20) ;
 		//d.WeightInPounds = 50;
 		
-		Dog smallDog;				//Declaration of a Dog variable
+		/**Dog smallDog;				//Declaration of a Dog variable
 		new Dog(20);				//Instantiation of the Dog class as a Dog Object!
 		smallDog = new Dog(5);		//Instantiation and assignment
 		Dog LargeDog = new Dog(150);	//Declaration,Instantiation and assignment
@@ -12,8 +12,17 @@ public class DogLauncher{
 		smallDog.classification();
 		LargeDog.makeNoise();
 		LargeDog.classification();
-		
+		*/
 		//d.makeNoise();
 		//Dog.makeNoise();
+		
+		Dog[] dogs= new Dog[2];
+		dogs[0] = Dog(20);
+		dogs[1] = Dog(150);
+		dogs[0].makeNoise();
+		dogs[0].classification();
+		dogs[1].makeNoise();
+		dogs[1].classification();
+		
 	}
 }
