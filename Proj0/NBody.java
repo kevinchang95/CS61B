45211539425c7e9905f2e6f7014a2b4d84a34294
kevinchang95 <kevinchanguh@gmail.com>
2019-09-10 22:19:59 -0500

    Add test println

--- Proj0/NBody.java
+++ Proj0/NBody.java
@@ -4,11 +4,9 @@ public class NBody{
 	public static double readRadius(String fileName){
 		
 		In in =  new In(fileName);
-		int bodys = in.readInt();
+		int numofBody = in.readInt();
 		double Radius = in.readDouble();
 		
-
-
 
 		return Radius;
 	
@@ -16,7 +14,36 @@ public class NBody{
 	
 
 
-
+	public static Body[] readBodies(String fileName){
+		In in = new In(fileName);
+		int numofBody = in.readInt();
+		double Radius = in.readDouble();
+		Body[] planets = new Body[numofBody];
+		
+		// System.out.println(numofBody);
+		// System.out.println(Radius);
+		
+		for(int i = 0;i < numofBody; i = i + 1){
+			
+			planets[i].xxPos = in.readDouble(); 
+			planets[i].yyPos = in.readDouble();
+			planets[i].xxVel = in.readDouble();
+			planets[i].yyVel = in.readDouble();
+			planets[i].mass  = in.readDouble();
+			planets[i].imgFileName = in.readString();
+			
+			System.out.println(planets[i].xxPos);
+			System.out.println(planets[i].yyPos);
+			System.out.println(planets[i].xxVel);
+			System.out.println(planets[i].yyVel);
+			System.out.println(planets[i].mass);
+			System.out.println(planets[i].imgFileName);
+		}
+		
+		
+		return planets;
+ 
+	}
 
 
 
