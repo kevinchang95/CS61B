4d2b3106605414ac464fb8f896676b8f606715f0
kevinchang95 <kevinchanguh@gmail.com>
2019-09-10 23:06:24 -0500

    Add middle variable

--- Proj0/NBody.java
+++ Proj0/NBody.java
@@ -19,19 +19,32 @@ public class NBody{
 		int numofBody = in.readInt();
 		double Radius = in.readDouble();
 		Body[] planets = new Body[numofBody];
+		Body b = new Body(0,0,0,0,0," ");
+		
 		
 		// System.out.println(numofBody);
 		// System.out.println(Radius);
 		
 		for(int i = 0;i < numofBody; i = i + 1){
 			
-			planets[i].xxPos = in.readDouble(); 
-			planets[i].yyPos = in.readDouble();
-			planets[i].xxVel = in.readDouble();
-			planets[i].yyVel = in.readDouble();
-			planets[i].mass  = in.readDouble();
-			planets[i].imgFileName = in.readString();
+			// planets[i].xxPos = in.readDouble(); 
+			// planets[i].yyPos = in.readDouble();
+			// planets[i].xxVel = in.readDouble();
+			// planets[i].yyVel = in.readDouble();
+			// planets[i].mass  = in.readDouble();
+			// planets[i].imgFileName = in.readString();
 			
+			b.xxPos = in.readDouble(); 
+			b.yyPos = in.readDouble();
+			b.xxVel = in.readDouble();
+			b.yyVel = in.readDouble();
+			b.mass  = in.readDouble();
+			b.imgFileName = in.readString();
+			
+			
+			planets[i] = b;
+			
+						
 			System.out.println(planets[i].xxPos);
 			System.out.println(planets[i].yyPos);
 			System.out.println(planets[i].xxVel);
