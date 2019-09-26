ce5d0c691ccd439842b1cdcb74ecf428f9c0e5f0
kevinchang95 <kevinchanguh@gmail.com>
2019-09-26 10:58:14 -0500

    Empty List, Size caching

--- Lecture2/2.2/SLList.java
+++ Lecture2/2.2/SLList.java
@@ -9,14 +9,23 @@ public class SLList {
     }
 
     private IntNode first; 
+	private int size;					//compute size at each methods
 
     public SLList(int x) {
         first = new IntNode(x, null);
+		size = 1;
     }
-
+	
+	public SLList(){					//Constructor for empty list
+		first = null;
+		size = 0;
+	}
+	
+	
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
         first = new IntNode(x, first);
+		size += 1;
     }    
 
     /** Retrieves the front item from the list. */
@@ -24,9 +33,12 @@ public class SLList {
         return first.item;
     }
 
-    /** Adds an item to the end of the list. */
-    
+    /** Adds an item to the end of the list. 
+	    This is an recursive method that uses 
+		nested method.*/
+		
 	private IntNode addLast(IntNode p, int x){
+		
 		if(p.next == null){
 			p.next = new IntNode(x,null);
 			return p;
@@ -37,27 +49,52 @@ public class SLList {
 	
 	public void addLast(int x) {
         /* Your Code Here! */
-	
-		IntNode p = first;
-		p = addLast(p, x);
-		
+		if(first == null){
+			first = new IntNode(x,null);
+		}else{
+			IntNode p = first;
+			p = addLast(p, x);
+		}
+		size += 1;
     }
 
-    /** Returns the number of items in the list using recursion. */
-    public int size() {
-        /* Your Code Here! */
-		if(first.next == null){
+
+	 /** Get the size of the SLList through traverse the list. 
+	    This is an recursive method that uses 
+		nested method.*/
+	/**private static int size(IntNode p){
+		if(p.next == null){
 			return 1;
 		}
-		return 1;
+		
+		
+		return 1 + size(p.next);
+		
+	
+	}*/
+		
+    /** Returns the number of items in the list using recursion. */
+    // public int size() {
+        // /* Your Code Here! */
+		
+		// return size(first);
+	// }
+	
+	
+	public int size(){
+	
+		return size;
 	}
 	
 	public static void main(String[] args){
-		SLList p = new SLList(10);
-		p.addFirst(5);
+		// SLList p = new SLList(10);
+		SLList p = new SLList();
+		// p.addFirst(5);
 		p.addLast(15);
 		p.addLast(20);
-		System.out.println(p.first.next.next.item);
+		System.out.println(p.first.next.item);
+		System.out.println(p.size());
+	
 	
 	}
 	
