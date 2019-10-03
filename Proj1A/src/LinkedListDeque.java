62a27306deca7a42897beca92cd2ba3de55a594b
kevinchang95 <kevinchanguh@gmail.com>
2019-10-03 14:27:00 -0500

    Proj1A part1 finished!

---
+++ Proj1A/src/LinkedListDeque.java
@@ -0,0 +1,146 @@
+import edu.princeton.cs.algs4.LookupIndex;
+
+public class LinkedListDeque<dataType> {
+    private Node sentinel;
+    private int size;
+
+
+    public class Node{
+        public Node prev;
+        public dataType item;
+        public Node next;
+
+        public Node(Node p, dataType i, Node n){
+
+            prev = p;
+            item = i;
+            next = n;
+
+        }
+
+    }
+
+    public LinkedListDeque(){
+        size = 0;
+
+        sentinel = new Node(null, null ,null);
+
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+
+    }
+
+    public LinkedListDeque(LinkedListDeque m){
+
+
+    }
+
+
+    /**List machinery
+     * The last Node is: sentinel.prev
+     * The first Node is: sentinel.next
+     * "last".next = sentinel
+     * @param q
+     */
+
+    public void addFirst(dataType q ){
+        Node p = new Node(null,q,null);
+        p.next = sentinel.next;
+        sentinel.next.prev = p;
+        sentinel.next = p;
+        p.prev = sentinel;
+        size +=1;
+    }
+
+    public void addLast(dataType q){
+        Node p = new Node(null,q,null);
+        //Link last Node with List
+        sentinel.prev.next= p;
+        p.prev = sentinel.prev;
+
+        //Move sentinel
+        p.next = sentinel;
+        sentinel.prev = p;
+
+
+        size +=1;
+    }
+
+    public Node removeFirst(){
+        Node p = sentinel.next;
+        if(p == sentinel){
+            return null;
+        }
+
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+
+        size -=1;
+        return p;
+    }
+
+    public Node removeLast(){
+
+        Node p = sentinel.prev;
+        if(p == sentinel){
+            return null;
+        }
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -=1;
+        return p;
+    }
+
+    public int size(){
+
+        return size;
+    }
+
+    public boolean isEmpty(){
+
+        if(size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public void printDeque(){
+        Node p = sentinel.next;
+        while(p != sentinel) {
+            System.out.print(p.item+" ");
+            p = p.next;
+        }
+        System.out.println();
+    }
+
+    public Node get(int i){
+        Node p = sentinel;
+        while(p.next !=sentinel){
+            if(i == 0){
+                return p;
+            }
+            p = p.next;
+            i--;
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
