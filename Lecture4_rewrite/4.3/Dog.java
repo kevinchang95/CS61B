209b9fea53bcf78b7ab3651b8e5359c747cec020
kevinchang95 <kevinchanguh@gmail.com>
2020-06-08 17:46:24 -0500

    Lec4.3: Add Comparator

--- Lecture4_rewrite/4.3/Dog.java
+++ Lecture4_rewrite/4.3/Dog.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 //public class Dog implements OurComparable{
 public class Dog implements Comparable<Dog>{
     private String name;
@@ -7,6 +8,29 @@ public class Dog implements Comparable<Dog>{
         name = n;
         size = s;
     }
+
+    public static class NameComparator implements Comparator<Dog>{
+
+
+        public int compare(Dog d1, Dog d2){
+
+            return d1.name.compareTo(d2.name);
+
+        }
+
+    }
+
+    public static class SizeComparator implements Comparator<Dog>{
+
+
+        public int compare(Dog d1, Dog d2){
+
+            return d1.size - d2.size;
+
+        }
+
+    }
+
 
     public void bark() {
         System.out.println(name + " says: bark");
--- Lecture4_rewrite/4.3/DogLauncher.java
+++ Lecture4_rewrite/4.3/DogLauncher.java
@@ -1,3 +1,4 @@
+import java.util.Comparator;
 public class DogLauncher {
     public static void main(String[] args) {
         Dog d1 = new Dog("Elyse", 3);
@@ -6,5 +7,20 @@ public class DogLauncher {
         Dog[] dogs = new Dog[]{d1, d2, d3};
         System.out.println(Maximizer.max(dogs));
         Dog d = (Dog) Maximizer.max(dogs);
+
+        Comparator<Dog> nameCompare = new Dog.NameComparator();
+        if(nameCompare.compare(d1,d2) > 0){
+
+            d1.bark();
+
+        }else d2.bark();
+
+        Comparator<Dog> sizeCompare = new Dog.SizeComparator();
+        if(sizeCompare.compare(d1,d2) > 0){
+
+            d1.bark();
+
+        }else d2.bark();
+
     }
 }
