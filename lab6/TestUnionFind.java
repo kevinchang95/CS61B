4617e11590014b7797ed5ab409441b4463258f74
kevinchang95 <kevinchanguh@gmail.com>
2020-06-17 18:04:05 -0500

    Lab6: Add tests

--- lab6/TestUnionFind.java
+++ lab6/TestUnionFind.java
@@ -19,14 +19,18 @@ public class TestUnionFind {
     @Test
     public void testConnect(){
 
-        UnionFind uf = new UnionFind(5);
-        uf.union(2,4);
-        assertTrue(uf.connected(2,4));
-        assertEquals(2,uf.sizeOf(2));
-        assertEquals(2,uf.sizeOf(4));
-        assertEquals(1,uf.sizeOf(1));
+        UnionFind uf = new UnionFind(9);
+        uf.union(0,1);
+        uf.union(1,8);
+        uf.union(2,0);
+        uf.union(3,4);
+        uf.union(5,4);
+        assertFalse(uf.connected(2,4));
+        assertEquals(4,uf.sizeOf(2));
+        assertEquals(3,uf.sizeOf(4));
+        assertEquals(1,uf.sizeOf(6));
         uf.union(4,2);
-        assertEquals(2,uf.sizeOf(4));
+        assertEquals(7,uf.sizeOf(4));
 
 
     }
